3bba815493b0bf7019b709cdde8c09ee38d9c0b7
“idkhashir” <hashirali12189@gmail.com>
2025-05-07 09:33:57 +0500

    Add new file

---
+++ Calender.py
@@ -0,0 +1,8 @@
+import calendar
+
+year = int(input("Enter a year : "))
+
+month = int(input("Enter a month : "))
+
+print()
+print(calendar.month(year, month))
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # First-Project
 This is my first Github project
+<br>
+Author = Hashir
